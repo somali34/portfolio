d5942727fe3bd5b6cfa9d26541381433cea683ca
somali34 <69794218+somali34@users.noreply.github.com>
2021-03-27 22:27:52 +0900

    Delete NEW-portfolio directory

--- NEW-portfolio/js/hamburger.js
+++
@@ -1,9 +0,0 @@
-$(".open-btn").click(function () {
-    $(this).toggleClass('active');
-});
-
-$(document).ready(function(){
-    $(".open-btn").click(function(){
-      $(".sp-nav").slideToggle();
-    });
-  });
